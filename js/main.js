82dbd2745f327f1191500431b4444e5b1c044028
ngraves7860@cps.edu <>
2014-08-14 19:52:57 +0000

    final flappy boxes

--- js/main.js
+++ js/main.js
@@ -11,52 +11,62 @@ var mainState = {
     //This function will execute at the beginning of the game
     //Here we'll load all of our assets (art, music, etc)
     
-    game.load.image('bird', 'assets/bird');
+    //Set the background color of the game
+    game.stage.backgroundColor = "#71c5cf";
     
     game.load.image('bird', 'assets/bird.png');
      
     game.load.image('pipe', 'assets/pipe.png');
+
+
 },
 
 
   create: function () {
   //The create function is calle right after the preload function
   //This is where we'll set up the game assets from scratch
-  
-  //Start our Physic Engine
+   
+ 
   game.physics.startSystem(Phaser.Physics.ARCADE);
   
-  this.bird = this.game.add.sprite(100, 250, 'bird');
+  this.bird = this.game.add.sprite(100, 245, 'bird');
   
   game.physics.arcade.enable(this.bird);
   
-  
   this.bird.body.gravity.y= 1000;
   
-  //add Pipes to the game
+  var spacekey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  
+  spacekey.onDown.add(this.jump, this);
+  
   this.pipes = game.add.group();
   
-  //Add the body to the group
-  this.pipes = game.add.group();
+  this.pipes.enableBody = true;
   
   //Create 20 pipes to hold in the group
   this.pipes.createMultiple(20, 'pipes')
   
   //Add in pipes over 1.5 seconds to the screen
-  this.timer = game.time.events.loo
+  this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
   
-  //When spacebar is pressed, make the bird jump!
-  var spaceKey = this.game.input.keyboard.adkey(Phaser.Keyboard.SPACEBAR);
-  spacekey.onDown.add(this.jump, this);
+  
+  
+  this.score = 0;
+  
+  this.labelScore = game.add.text(20,20, "0", {font: "30px Arial", fill: "#ffffff"});
 },
 
   update: function () {
   //This function is called 60 times a second
-  //it contains the games logic and all tie related actions
+  
   
   if (this.bird.inWorld == false) {
-    this.restartGame();  
-   }
+    this.restartGame();
+  }
+  
+  game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
+
+   
 },
 
   addOnePipe: function (x,y) {
@@ -69,8 +79,9 @@ var mainState = {
     //Move the pipes to the left of the screen
     pipe.body.velocity.x = -200;
     
-    //Kill the pipe its off
-  },
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+},
 
   addRowOfPipes: function () {
   var hole = Math.floor(Math.random() * 5) + 1;
@@ -81,16 +92,25 @@ var mainState = {
     if(i !=hole && i !=hole + 1) {
 
       this.addOnePipe(400, i*60 + 10);  
+      
     }
+    
+    this.score += 1;
+    this.labelScore.text = this.score;
 },
   
   jump: function () {
     this.bird.body.velocity.y =-350;
-  }
+},
 
+  restartGame: function () {
+  
+    game.state.start('main');
+  
+},
 
+};
 
-}
 
 //Add and start the gameState
 game.state.add('main', mainState);
